eacbbbe5e5c91224078ebacdb1fc23c781830eb3
Rowan-James Tran <rowanjames.tran@gmail.com>
2024-12-02 01:02:51 -0800

    add timer for solutions

--- 2024/util/run_parts.go
+++ 2024/util/run_parts.go
@@ -3,16 +3,29 @@ package util
 import (
 	"flag"
 	"fmt"
+	"strings"
+	"time"
 )
 
+func divider() {
+	fmt.Println(strings.Repeat("-", 40))
+}
+
 func RunChosenPart(part1Handler func(), part2Handler func()) {
-	part := flag.Int("part", 1, "either 1 (default) or 2")
+	part := flag.Int("part", 1, "either 1 or 2")
 	flag.Parse()
 
 	fmt.Println("running part", *part)
+	divider()
+
+	start := time.Now()
 	if *part == 1 {
 		part1Handler()
 	} else {
 		part2Handler()
 	}
+	runtime := time.Since(start)
+
+	divider()
+	fmt.Println("took", runtime)
 }
