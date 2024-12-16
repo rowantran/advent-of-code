34520389603d5d557cf345d1b2d032c2dda63097
Rowan-James Tran <rowanjames.tran@gmail.com>
2024-12-15 19:06:21 -0800

    day11 solution
    spent so much time trying to overthink a number theory solution instead
    of just running this solution first...

---
+++ 2024/solutions/day11/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	_ "embed"
+
+	"github.com/rowantran/advent-of-code/2024/util"
+)
+
+//go:embed input
+var input string
+
+type PuzzleInput struct {
+	counts map[int64]int
+}
+
+func Parse(input string) PuzzleInput {
+	var problem PuzzleInput
+	problem.counts = make(map[int64]int)
+	for _, num := range strings.Fields(input) {
+		parsedNum := util.MustAtoiInt64(num)
+		problem.counts[parsedNum]++
+	}
+	return problem
+}
+
+func solve(p PuzzleInput, iterations int) int {
+	for range iterations {
+		// stone transformations are "simultaneous" so we can't mutate the counts map in-place
+		deltas := make(map[int64]int)
+		
+		for k, v := range p.counts {
+			deltas[k] -= v
+
+			switch {
+			case k == 0:
+				deltas[1] += v
+			case util.DigitCountInt64(k) % 2 == 0:
+				left, right := splitNum(k)
+				deltas[left] += v
+				deltas[right] += v
+			default:
+				deltas[2024*k] += v
+			}
+		}	
+
+		for k, v := range deltas {
+			p.counts[k] += v
+			if p.counts[k] == 0 {
+				delete(p.counts, k)
+			}
+		}
+
+		//fmt.Printf("completed iteration #%d: %v\n", i+1, p.counts)
+	}
+	
+	sum := 0
+	for _, v := range p.counts {
+		sum += v
+	}
+	return sum
+}
+
+// split an integer with even number of digits into left and right halves
+func splitNum(n int64) (int64, int64) {
+	digits := util.DigitCountInt64(n)
+	div := util.ExpInt64(10, digits/2)
+	return n / div, n % div
+}
+
+func part1() {
+	problem := Parse(input)
+	//fmt.Println(problem)
+	ans := solve(problem, 25)
+	fmt.Println("answer:", ans)
+}
+
+func part2() {
+	problem := Parse(input)
+	ans := solve(problem, 75)
+	fmt.Println("answer:", ans)
+}
+
+func main() {
+	util.RunChosenPart(part1, part2)
+}
--- 2024/util/int.go
+++ 2024/util/int.go
@@ -10,6 +10,14 @@ func MustAtoi(s string) int {
 	return i
 }
 
+func MustAtoiInt64(s string) int64 {
+	n, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
 func Abs(i int) int {
 	if (i < 0) {
 		return -i
@@ -20,4 +28,38 @@ func Abs(i int) int {
 
 func RuneToInt(r rune) int {
 	return int(r - '0')
+}
+
+func DigitCountInt64(a int64) int {
+	var p int = 0
+	var base int64 = 1
+
+	if a == 0 {
+		return 1
+	}
+
+	for base <= a {
+		p++
+		base *= 10
+	}
+
+	// p is the smallest int s.t. 10^p > a
+	return p
+}
+
+func DigitCount(a int) int {
+	return DigitCountInt64(int64(a))
+}
+
+// calculate b to the nth power
+func ExpInt64(b int64, n int) int64 {
+	res := int64(1)
+	for range n {
+		res *= b
+	}
+	return res
+}
+
+func ExpInt(b int, n int) int {
+	return int(ExpInt64(int64(b), n))
 }
