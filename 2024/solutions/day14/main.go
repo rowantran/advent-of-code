84cd35bb0ca84c8a421557f6867e7893c033b6cd
Rowan-James Tran <rowanjames.tran@gmail.com>
2024-12-24 22:58:56 -0800

    day14: part 2 solution

--- 2024/solutions/day14/main.go
+++ 2024/solutions/day14/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
 	"strings"
 
 	_ "embed"
@@ -23,8 +28,8 @@ func ParseRobot(line string) Robot {
 	return Robot{util.NewVec2Int(fields[0][2:]), util.NewVec2Int(fields[1][2:])}
 }
 
-func (r Robot) FinalQuadrant() ([2]bool, error) {
-	fp := r.finalPos()
+func (r Robot) FinalQuadrant(seconds int) ([2]bool, error) {
+	fp := r.FinalPos(seconds)
 	x, y := fp.Parts()
 	if x == width/2 || y == height/2 {
 		return [2]bool{}, fmt.Errorf("final position was in center row or column")
@@ -32,8 +37,8 @@ func (r Robot) FinalQuadrant() ([2]bool, error) {
 	return [2]bool{x > width/2, y > height/2}, nil
 }
 
-func (r Robot) finalPos() Vec2 {
-	unwrappedPos := r.startPos.Add(r.velocity.Mul(iterations))
+func (r Robot) FinalPos(seconds int) Vec2 {
+	unwrappedPos := r.startPos.Add(r.velocity.Mul(seconds))
 	finalPos := Vec2{mod(unwrappedPos[0], width), mod(unwrappedPos[1], height)}
 	//fmt.Printf("final pos for robot with startPos %v, vel %v = %v\n", r.startPos, r.velocity, finalPos)
 	return finalPos
@@ -57,10 +62,68 @@ func Parse(input string) PuzzleInput {
 	return problem
 }
 
-func solve(p PuzzleInput, isPart2 bool) int64 {
+type PuzzleImage struct {
+	p PuzzleInput
+	seconds int
+	imageCache map[int][][]bool
+}
+
+func (pi *PuzzleImage) ColorModel() color.Model {
+	return color.GrayModel
+}
+
+func (pi *PuzzleImage) Bounds() image.Rectangle {
+	return image.Rect(0, 0, width, height)
+}
+
+func (pi *PuzzleImage) At(x, y int) color.Color {
+	pi.cacheImage()
+	if pi.imageCache[pi.seconds][x][y] {
+		return color.Gray{255}
+	} else {
+		return color.Gray{0}
+	}
+}
+
+func (pi *PuzzleImage) WriteImageToFile() {
+	filename := fmt.Sprintf("out/image_%04d.png", pi.seconds)
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatalln("failed to open image for writing", err)
+	}
+
+	if err := png.Encode(f, pi); err != nil {
+		f.Close()
+		log.Fatalln("failed to encode image", err)
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatalln("failed to close image", err)
+	}
+
+	fmt.Println("wrote image to", filename)
+}
+
+func (pi *PuzzleImage) cacheImage() {
+	if _, ok := pi.imageCache[pi.seconds]; ok {
+		return
+	}
+
+	pi.imageCache[pi.seconds] = make([][]bool, width)
+	for r := range width {
+		pi.imageCache[pi.seconds][r] = make([]bool, height)
+	}
+
+	for _, r := range pi.p.robots {
+		x, y := r.FinalPos(pi.seconds).Parts()
+		pi.imageCache[pi.seconds][x][y] = true
+	}
+}
+
+func solve(p PuzzleInput) int64 {
 	quadrantCounts := make(map[[2]bool]int)
 	for _, r := range p.robots {
-		quad, err := r.FinalQuadrant()
+		quad, err := r.FinalQuadrant(100)
 		if err == nil {
 			quadrantCounts[quad]++
 		}
@@ -73,17 +136,43 @@ func solve(p PuzzleInput, isPart2 bool) int64 {
 	return int64(ans)
 }
 
+func writeAllImages(p PuzzleInput) {
+	image := PuzzleImage{
+		p: p,
+		imageCache: make(map[int][][]bool),
+	}
+	for i := range max(width, height) {
+		image.seconds = i
+		image.WriteImageToFile()
+	}
+
+	image.seconds = t
+	image.WriteImageToFile()
+}
+
 //go:embed input
 var input string
 
-const iterations = 100
 //const width, height = 11, 7
 const width, height = 101, 103
+const t = 7861
+
+// results from investigating part 2 images:
+// image_0033 has dots clustered in rows
+// image_0084 has dots clustered in columns
+// using insight from reddit, find the desired time t such that
+// t = 33 mod 103
+// t = 84 mod 101
+// CRT calculator shows t = 7861
 
 func main() {
 	util.SolveChosenPart(func(isPart2 bool) int64 {
 		problem := Parse(input)
-		//fmt.Println(problem)
-		return solve(problem, isPart2)
+		if isPart2 {
+			writeAllImages(problem)
+			return 0
+		} else {
+			return solve(problem)
+		}
 	})
 }
