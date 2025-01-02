8357335e4dbdfba1679d60e156fb68413c547fa9
Rowan-James Tran <rowanjames.tran@gmail.com>
2025-01-01 22:48:20 -0800

    day16: working part 1 solution

---
+++ 2024/solutions/day16/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"container/heap"
+	"math"
+
+	_ "embed"
+
+	"github.com/rowantran/advent-of-code/2024/util"
+)
+
+type Vec2 = util.Vec2[int]
+var directions = []Vec2{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+
+type PuzzleInput struct {
+	maze util.Grid[rune]
+	start Vec2
+	end Vec2
+}
+
+func Parse(input string) PuzzleInput {
+	var p PuzzleInput
+	p.maze = util.NewGridFromString(input, func (r rune, pos Vec2) rune {
+		if r == 'S' {
+			p.start = pos
+		} else if r == 'E' {
+			p.end = pos
+		}
+		return r
+	})
+	return p
+}
+
+func (p PuzzleInput) Neighbors(pos Vec2) []Vec2{
+	neighbors := make([]Vec2, 0)
+	for _, dir := range directions {
+		n := pos.Add(dir)
+		if p.maze.InBounds(n) && p.maze.Get(n) != '#' {
+			neighbors = append(neighbors, n)
+		}
+	}
+	return neighbors
+}
+
+type PuzzleNode struct {
+	location Vec2
+	direction Vec2
+}
+
+type PuzzleNodeHeapItem struct {
+	node PuzzleNode
+	distance int
+}
+
+func solve(p PuzzleInput, isPart2 bool) int64 {
+	dists, prevs := dijkstra(p)
+
+	if !isPart2 {
+		res := math.MaxInt
+		for _, dir := range directions {
+			res = min(res, dists[PuzzleNode{p.end, dir}])
+		}
+		return int64(res)
+	} else {
+		tiles := make(util.Set[Vec2])		
+		tracePaths(prevs, PuzzleNode{p.start, Vec2{0,1}}, tiles)
+		return int64(tiles.Size())
+	}
+}
+
+// collect all tiles on path(s) to the target node
+func tracePaths(prevs map[PuzzleNode][]PuzzleNode, node PuzzleNode, tiles util.Set[Vec2]) {
+	for _, prev := range prevs[node] {
+		tiles.Add(prev.location)
+		tracePaths(prevs, prev, tiles)
+	}
+}
+
+func dijkstra(p PuzzleInput) (map[PuzzleNode]int, map[PuzzleNode][]PuzzleNode) {
+	dists := make(map[PuzzleNode]int)
+	prevs := make(map[PuzzleNode][]PuzzleNode)
+	pq := util.NewHeap(func(a, b PuzzleNodeHeapItem) bool { return a.distance < b.distance })
+
+	for r := range len(p.maze) {
+		for c := range len(p.maze[r]) {
+			for _, dir := range directions {
+				if p.maze[r][c] == '#' { continue }
+
+				dist := math.MaxInt
+				if (p.maze[r][c] == 'S' && dir == Vec2{0, 1}) {
+					dist = 0
+				}
+				node := PuzzleNode{Vec2{r,c}, dir}
+				dists[node] = dist
+				heap.Push(&pq, PuzzleNodeHeapItem{node, dist})
+			}
+		}
+	}
+
+	for pq.Len() > 0 {
+		node := heap.Pop(&pq).(PuzzleNodeHeapItem).node
+		neighbors := []PuzzleNode{}
+
+		// build list of neighbors
+		forward := node.location.Add(node.direction)
+		if p.maze.InBounds(forward) && p.maze.Get(forward) != '#' {
+			neighbors = append(neighbors, PuzzleNode{forward, node.direction})
+		}
+		for _, dir := range directions {
+			if node.direction != dir && node.direction != dir.Mul(-1) {
+				neighbors = append(neighbors, PuzzleNode{node.location, dir})
+			}
+		}
+
+		// check neighbors
+		for _, neighbor := range neighbors {
+			var edgeCost int
+			if neighbor.direction == node.direction {
+				// forward move
+				edgeCost = 1
+			} else {
+				// 90 degree turn
+				edgeCost = 1000
+			}
+			totalCost := dists[node] + edgeCost
+			if totalCost < dists[neighbor] {
+				pq.Update(PuzzleNodeHeapItem{neighbor, dists[neighbor]}, PuzzleNodeHeapItem{neighbor, totalCost})
+				dists[neighbor] = totalCost
+				prevs[neighbor] = []PuzzleNode{node}
+			} else if totalCost == dists[neighbor] {
+				prevs[neighbor] = append(prevs[neighbor], node)
+			}
+		}
+	}
+
+	return dists, prevs
+}
+
+//go:embed input
+var input string
+
+func main() {
+	util.SolveChosenPart(func(isPart2 bool) int64 {
+		problem := Parse(input)
+		//fmt.Println(problem)
+		return solve(problem, isPart2)
+	})
+}
---
+++ 2024/util/heap.go
@@ -0,0 +1,47 @@
+package util
+
+import "container/heap"
+
+type Heap[T comparable] struct {
+	data []T
+	indices map[T]int
+	lessFunc func(T, T) bool
+}
+
+func NewHeap[T comparable](lessFunc func(T, T) bool) Heap[T] {
+	return Heap[T]{
+		data: make([]T, 0),
+		indices: make(map[T]int),
+		lessFunc: lessFunc,
+	}
+}
+
+func (h *Heap[T]) Len() int           { return len(h.data) }
+func (h *Heap[T]) Less(i, j int) bool { return h.lessFunc(h.data[i], h.data[j]) }
+func (h *Heap[T]) Swap(i, j int) {
+	h.data[i], h.data[j] = h.data[j], h.data[i]
+	h.indices[h.data[i]] = i
+	h.indices[h.data[j]] = j
+}
+func (h *Heap[T]) Push(x any) {
+	h.indices[x.(T)] = len(h.data)
+	h.data = append(h.data, x.(T))
+}
+func (h *Heap[T]) Pop() any {
+	v := h.data[len(h.data)-1]
+	h.data = h.data[:len(h.data)-1]
+	h.indices[v] = -1
+	return v
+}
+func (h *Heap[T]) Update(val T, newVal T) {
+	i := h.indices[val]
+	if i == -1 {
+		// val is not in heap
+		return
+	}
+
+	h.data[i] = newVal
+	h.indices[val] = -1
+	h.indices[newVal] = i
+	heap.Fix(h, i)
+}
